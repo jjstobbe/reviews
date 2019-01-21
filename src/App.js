d56efe50f480ae79da33297ac2ecf0699754f911
Jack <jjstobbe@gmail.com>
2019-01-21 16:57:38 -0600

    Messing with goodreads API, why does it return XML

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import parser from 'xml2json';
+
+
+var keys = {
+  "goodreads": // <KEY> //
+}
+
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
 class App extends Component {
+  searchGoodreads = (query) => {
+    fetch(`${proxyurl}https://www.goodreads.com/search?q=${query}&key=Anz5YTfUR2W75yBAFYP3w`,
+    {
+      method: "GET",
+      mode: "cors",
+      headers: new Headers({
+        'Access-Control-Allow-Origin':'*'
+      })
+    })
+      .then(response => response.text())
+      .then(XML => parser.toJson(parser))
+      .then(resp => console.log(resp))
+      .catch(function(err) {
+        console.error(err);
+      });
+  }
+
   render() {
+    var result = this.searchGoodreads("Help");
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {}
       </div>
     );
   }
